734a02a2451eb00871bcf330c26f0424e25ae7f1
Luana-SMatos <luana.smatos01@gmail.com>
2024-05-27 14:57:51 -0300

    subscribe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,8 +107,14 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+      <section id="subscribe" className="padding-x sm:py-32 py-16 w-full max-container flex justify-between items-center max-lg:flex-col gap-10">
+        <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up For <span className="text-my-blue">Updates</span> & Newsletter</h3>
+        <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2 sm:border sm:border-slate-400 rounded-full">
+          <input className="input" type="text" placeholder="subscribe@nike.com" />
+          <div className="flex max-sm:justify-end items-center max-sm:w-full">
+            <Button label="Sign Up"></Button>
+          </div>
+        </div>
       </section>
 
       <section className="padding-x bg-black padding-t pb-8">
